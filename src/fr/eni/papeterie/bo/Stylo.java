b450a75d32249fc6d1db0aca333d6f5223eb21b6
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2020-11-24 10:57:56 +0100

    creating an article

---
+++ src/fr/eni/papeterie/bo/Stylo.java
@@ -0,0 +1,39 @@
+package fr.eni.papeterie.bo;
+
+public class Stylo extends Article {
+	
+	private String couleur;
+	
+	public Stylo(int idArticle, String reference, String marque, String designation, 
+			float prixUnitaire, int qteStock) {
+		super(idArticle, reference, marque, designation, prixUnitaire, qteStock);
+	}
+
+	public Stylo(int idArticle, String reference, String marque, String designation, 
+			float prixUnitaire, int qteStock, String couleur) {
+		super(idArticle, reference, marque, designation, prixUnitaire, qteStock);
+		this.couleur = couleur;
+	}
+	
+	public Stylo(String reference, String marque, String designation, 
+			float prixUnitaire, int qteStock, String couleur) {
+		super(reference, marque, designation, prixUnitaire, qteStock);
+		this.couleur = couleur;
+	}
+
+	public String getCouleur() {
+		return couleur;
+	}
+
+	public void setCouleur(String couleur) {
+		this.couleur = couleur;
+	}
+
+	@Override
+	public String toString() {
+		return "Stylo [couleur=" + couleur + "]";
+	}
+	
+	
+
+}
